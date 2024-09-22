9a729a326b4437f67132df987501d9291bd11a5d
CarolPatricio <carolpatricio28@hotmail.com>
2024-09-22 00:32:19 -0300

    Finalizando o cadastro de metas

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,14 +1,25 @@
 
 import { Dialog } from './components/ui/dialog'
 import { CreateGoal } from './components/create-goal'
+import { useEffect, useState } from 'react'
+import { EmptyGoals } from './components/ui/empty-goals'
 import { Summary } from './components/summary'
+import { useQuery } from '@tanstack/react-query'
+import { getSummary } from './http/get-summary'
+// import { Summary } from './components/summary'
 // import { EmptyGoals } from './components/ui/empty-goals'
 
+
 export function App() {
+  const { data } =  useQuery({
+    queryKey:['sumary'],
+    queryFn: getSummary,
+    staleTime: 1000 * 60 //60 seconds
+  })
+
   return (
     <Dialog>
-      <Summary/>
-        {/* <EmptyGoals/> */}
+        {data && data.total > 0 ? <Summary/> :  <EmptyGoals/>}
         <CreateGoal/>
     </Dialog>
   )
---
+++ web/src/http/get-summary.ts
@@ -0,0 +1,30 @@
+type summaryResponse = {
+	completed: number;
+	total: number;
+	goalsPerDay: Record<
+		string,
+		{
+			id: string;
+			title: string;
+			completedAt: string;
+		}[]
+	>;
+};
+
+export async function getSummary(): Promise<summaryResponse> {
+	const response = await fetch("http://localhost:3333/summary");
+	const data = await response.json();
+	return data.summary;
+}
+
+// const [summary, setSummary] = useState<SummaryResponse | null >(null)
+
+// useEffect(() => {
+//   fetch('http://localhost:3333/summary')
+//   .then(response => {
+//     return response.json()
+//   })
+//   .then(data => {
+//     setSummary(data.summary)
+//   })
+// }, [])
